00ce33320f058acc47ef2b30d2a21cfb1d21e7f5
PavelKrastev-hub <pavel__krustev@abv.bg>
2026-04-24 18:53:00 +0300

    Add get user by id method in post service

--- demo-workshop-typescript/src/services/posts.service.ts
+++ demo-workshop-typescript/src/services/posts.service.ts
@@ -6,4 +6,14 @@ export class PostService extends APIService<Post> {
     constructor() {
         super(`${BASE_URL}/posts`);
     }
+
+    async getPostsByUser(userId: number): Promise<Post[]> {
+        const response = await fetch(`${BASE_URL}/posts?userId=${userId}`);
+
+        if (!response.ok) {
+            throw new Error("Failed to fetch user posts");
+        }
+
+        return await response.json();
+    }
 }
