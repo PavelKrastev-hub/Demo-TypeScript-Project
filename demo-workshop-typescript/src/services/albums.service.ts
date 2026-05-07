65bd87d36f1cdd05e6d2aee3d994f3424fa54b2a
PavelKrastev-hub <pavel__krustev@abv.bg>
2026-05-07 12:37:47 +0300

    Create function getAlbumByUser in album service

--- demo-workshop-typescript/src/services/albums.service.ts
+++ demo-workshop-typescript/src/services/albums.service.ts
@@ -6,4 +6,14 @@ export class AlbumsService extends APIService<Album> {
     constructor() {
         super(`${BASE_URL}/albums`);
     }
+
+    async getAlbumsByUser(userId: number): Promise<Album[]> {
+        const response = await fetch(`${BASE_URL}/albums?userId=${userId}`);
+
+        if (!response.ok) {
+            throw new Error("Failed to fetch user albums");
+        }
+
+        return await response.json();
+    }
 }
