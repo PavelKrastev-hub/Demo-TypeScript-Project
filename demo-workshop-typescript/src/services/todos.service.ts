ec386d5af2ae52f8d225aaa7024323f9b21539b7
PavelKrastev-hub <pavel__krustev@abv.bg>
2026-05-07 13:23:29 +0300

    Create function getTodoByUser in todosService

--- demo-workshop-typescript/src/services/todos.service.ts
+++ demo-workshop-typescript/src/services/todos.service.ts
@@ -6,4 +6,14 @@ export class ToDosService extends APIService<ToDo> {
     constructor() {
         super(`${BASE_URL}/todos`);
     }
+
+    async getToDosByUser(userId: number): Promise<ToDo[]> {
+        const response = await fetch(`${BASE_URL}/todos?userId=${userId}`);
+
+        if (!response.ok) {
+            throw new Error("Failed to fetch user todos");
+        }
+
+        return await response.json();
+    }
 }
