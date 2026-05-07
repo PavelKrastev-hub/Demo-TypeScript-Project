66f97860fa188b1906b4355e68eb443cc37e2954
PavelKrastev-hub <pavel__krustev@abv.bg>
2026-05-07 12:38:13 +0300

    Create album view with css and render on profile content

---
+++ demo-workshop-typescript/src/views/albums.view.ts
@@ -0,0 +1,29 @@
+import type { Album } from "../interfaces/album.interface";
+import { AlbumsService } from "../services/albums.service";
+import { renderProfileContent } from "../utils/html";
+
+const albumService = new AlbumsService();
+
+export async function showUsersAlbums(usreId: number) {
+    const albums = await albumService.getAlbumsByUser(usreId);
+
+    const template = `
+     <div class="albums-container">
+            ${albums.map(album => generateSingleAlbum(album)).join("")}
+        </div>
+    `
+
+    renderProfileContent(template);
+}
+
+function generateSingleAlbum(album: Album) {
+    return `
+    <div class="album-card" >
+        <h3 class="album-title" > ${album.title} </h3>
+            <a href = "#" class="photos-btn" data - id="${album.id}" >
+            View Photos
+            </a>
+        <div class="photos-container" id = "photos-${album.id}" > </div>
+    </div>
+    `
+}
