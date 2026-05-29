88e974fb8ec5f60c41b66701d1df83126f71a6d0
PavelKrastev-hub <pavel__krustev@abv.bg>
2026-05-29 16:03:57 +0300

    Create getPhotosByAlbum function in service

--- demo-workshop-typescript/src/services/photos.service.ts
+++ demo-workshop-typescript/src/services/photos.service.ts
@@ -6,4 +6,14 @@ export class PhotosService extends APIService<Photo> {
     constructor() {
         super(`${BASE_URL}/photos`);
     }
+
+    async getPhotosByAlbum(albumId: number): Promise<Photo[]> {
+        const response = await fetch(`${BASE_URL}/photos?albumId=${albumId}`);
+
+        if (!response.ok) {
+            throw new Error("Failed to fetch post comments");
+        }
+
+        return await response.json();
+    }
 }
