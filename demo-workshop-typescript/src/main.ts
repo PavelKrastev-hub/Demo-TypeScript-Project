ebdeab3f0d6fd6c995cd3b868e18219e436d0820
PavelKrastev-hub <pavel__krustev@abv.bg>
2026-05-29 16:07:10 +0300

    Update main.ts

--- demo-workshop-typescript/src/main.ts
+++ demo-workshop-typescript/src/main.ts
@@ -1,6 +1,26 @@
+import type { Routes } from './interfaces/routes.interface';
+import { Router } from './router';
 import './style.css'
 import { renderUsersView } from './views/users.view'
 
 window.addEventListener('load', () => {
     renderUsersView();
 })
+
+const routes: Routes = {
+
+}
+
+const router = new Router(routes)
+
+export function setupLinks(id: number) {
+    const profileLinkEl = document.getElementById('view-btn');
+
+    if (profileLinkEl) {
+        profileLinkEl.addEventListener('click', e => {
+            e.preventDefault();
+        });
+
+        router.navigate(`/users/${id}`)
+    }
+}
