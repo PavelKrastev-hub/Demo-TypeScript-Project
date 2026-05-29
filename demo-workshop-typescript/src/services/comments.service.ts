34d7431214fba489592aa861cab3027bcab21339
PavelKrastev-hub <pavel__krustev@abv.bg>
2026-05-29 15:05:11 +0300

    Create getCommentsByPost function in service

--- demo-workshop-typescript/src/services/comments.service.ts
+++ demo-workshop-typescript/src/services/comments.service.ts
@@ -6,4 +6,14 @@ export class CommentService extends APIService<Comment> {
     constructor() {
         super(`${BASE_URL}/comments`);
     }
+
+    async getCommentsByPost(postId: number): Promise<Comment[]> {
+        const response = await fetch(`${BASE_URL}/comments?postId=${postId}`);
+
+        if (!response.ok) {
+            throw new Error("Failed to fetch post comments");
+        }
+
+        return await response.json();
+    }
 }
