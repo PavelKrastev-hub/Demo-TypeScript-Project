306c071883da60c343506c2c90be7a76a1570bd3
PavelKrastev-hub <pavel__krustev@abv.bg>
2026-05-29 16:05:02 +0300

    Update album view url and create users events

--- demo-workshop-typescript/src/views/albums.view.ts
+++ demo-workshop-typescript/src/views/albums.view.ts
@@ -1,6 +1,7 @@
 import type { Album } from "../interfaces/album.interface";
 import { AlbumsService } from "../services/albums.service";
 import { renderProfileContent } from "../utils/html";
+import { showAlbumPhotos } from "./photos.view";
 
 const albumService = new AlbumsService();
 
@@ -14,16 +15,32 @@ export async function showUserAlbums(usreId: number) {
     `
 
     renderProfileContent(template);
+    attachUserEvents(usreId)
 }
 
 function generateSingleAlbum(album: Album) {
     return `
     <div class="album-card" >
         <h3 class="album-title" > ${album.title} </h3>
-            <a href = "#" class="photos-btn" data - id="${album.id}" >
+            <a href = "#" class="photos-btn" data-id="${album.id}" >
             View Photos
             </a>
         <div class="photos-container" id = "photos-${album.id}" > </div>
     </div>
     `
+}
+
+function attachUserEvents(userId: number) {
+    const buttons = document.querySelectorAll('.photos-btn');
+
+    buttons.forEach(button => button?.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const target = e.currentTarget as HTMLAnchorElement;
+        const albumId = Number(target.dataset.id);
+
+        history.pushState({}, '', `/users/${userId}/album/${albumId}/photos`);
+
+        showAlbumPhotos(albumId, userId);
+    }));
 }
