bbd4fb4916f00c550e483bac89db770e4a3a568f
PavelKrastev-hub <pavel__krustev@abv.bg>
2026-05-29 15:04:45 +0300

    Create comment view html template and css

---
+++ demo-workshop-typescript/src/views/comments.view.ts
@@ -0,0 +1,48 @@
+import type { Comment } from "../interfaces/comment.interface";
+import { CommentService } from "../services/comments.service";
+import { renderProfileContent } from "../utils/html";
+import { showUsersPosts } from "./posts.view";
+
+const commentsService = new CommentService();
+
+export async function showPostComments(postId: number, userId: number) {
+    const comments = await commentsService.getCommentsByPost(postId);
+
+    const template = `
+        <a href="" class="back-btn">Back to Posts</a>
+        <div class="comments-wrapper">
+            ${comments.map(comment => generateSingleComment(comment)).join("")}
+        </div>
+    `
+
+    renderProfileContent(template);
+    attachCommentEvents(userId);
+}
+
+function generateSingleComment(comment: Comment) {
+    return `
+        <div class="comment-card">
+            <div class="comment-header">
+                <h4 class="comment-name">${comment.name}</h4>
+                <span class="comment-email">
+                    ${comment.email}
+                </span>
+            </div>
+            <p class="comment-body">
+                ${comment.body}
+            </p>
+        </div>
+    `
+}
+
+export function attachCommentEvents(userId: number) {
+    const button = document.querySelector('.back-btn');
+
+    button?.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        history.pushState({}, '', `/users/${userId}`);
+
+        showUsersPosts(userId);
+    })
+}
